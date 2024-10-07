4174dde7ffd00b032640f4f664f08709390041f8
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2024-10-07 01:38:06 -0300

    feat: Adicionando e configurando tailwindcss e nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="w-full h-full flex justify-center items-center">
+      <Text>Hello React Native!</Text>
     </View>
   );
 }
