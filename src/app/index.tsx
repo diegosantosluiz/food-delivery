d4e7be4be239ded253e13502e74215c38f0f1372
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2024-10-07 22:11:08 -0300

    feat: Criado componente para apresentar Banner

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import { ScrollView, View } from "react-native";
 import { Header } from "../components/header";
+import { Banner } from "../components/banner";
 
 import Constants from "expo-constants";
 const statusBarHeight = Constants.statusBarHeight + 10;
@@ -12,6 +13,7 @@ export default function Index() {
     >
       <View className="w-full px-4" style={{ marginTop: statusBarHeight }}>
         <Header />
+        <Banner />
       </View>
     </ScrollView>
   );
